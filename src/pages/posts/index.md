e3017ebccd4f7c35df4512e4a2d1ae110632fb39
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-06-25 02:00:17 +0200

    first push

---
+++ src/pages/posts/index.md
@@ -0,0 +1,19 @@
+---
+setup: |
+  import Layout from '../../layouts/BlogPost.astro'
+  import Cool from '../../components/Author.astro'
+title: Moving to Astro from Handlebars.
+publishDate: Thu 23 Jun. 2021 · 20.20
+name: Michael Andreuzza
+value: 128
+description: Converting my projects to Astro!
+---
+
+<Cool name={frontmatter.name} href="https://twitter.com/mike_andreuzza" client:load />
+
+# H1 - The quick brown fox jumps over the lazy dog
+## H2 - The quick brown fox jumps over the lazy dog
+### H3 - The quick brown fox jumps over the lazy dog
+#### H4 - The quick brown fox jumps over the lazy dog
+
+###### P - The quick brown fox jumps over the lazy dog
---
+++ tailwind.config.cjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        black: "#0d0d0d",
+		  },
+		fontFamily: {
+
+        display: [
+          '"Lexend"',
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ],
+
+      },
+
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
+}
